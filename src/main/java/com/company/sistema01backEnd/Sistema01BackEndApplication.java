7500104f311b865a0b2e6b0ab5ab873c514b6271
JoaoFork <jvictorsimao@hotmail.com>
2021-03-27 21:32:44 -0300

    initial commit

---
+++ src/main/java/com/company/sistema01backEnd/Sistema01BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.company.sistema01backEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sistema01BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sistema01BackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/sistema01backEnd/Sistema01BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.sistema01backEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sistema01BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
